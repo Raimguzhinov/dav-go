b51f82988ec9988d34540f75287f8ba60e64df2f
Raimguzhinov <d.raimguzhinov@stud.sibguti.ru>
2024-05-04 19:26:41 +0700

    Rename configs folder

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -3,12 +3,12 @@ package main
 import (
 	"log"
 
-	"github.com/Raimguhinov/dav-go/config"
+	"github.com/Raimguhinov/dav-go/configs"
 	"github.com/Raimguhinov/dav-go/internal/app"
 )
 
 func main() {
-	cfg, err := config.NewConfig()
+	cfg, err := configs.NewConfig()
 	if err != nil {
 		log.Fatalf("Config error: %s", err)
 	}
